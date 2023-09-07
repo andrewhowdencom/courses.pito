5bbf7ec790c0e2d233530ee876793496183f0131
Andrew Howden <hello@andrewhowden.com>
2023-09-07 16:38:18 +0200

    Mock the implementation of main
    This commit implements a minimal set of capabilities around the "main"
    function. This is just designed to be useful for me to write the logging
    section of the course — I will come back to the details on it.
    
    I probably should pause after logging and write the rest of this
    application out. I think I will need it continually.

--- delivery-service/main.go
+++ delivery-service/main.go
@@ -1,7 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/andrewhowdencom/courses.pito/delivery-service/server"
+)
+
+// flags that influence the programs behavior
+var addr = flag.String("a", "localhost:9093", "the address on which the server should listen")
 
 func main() {
-	fmt.Println("Hello, world")
+	// Bind signal handlers
+	ch := make(chan os.Signal, 1)
+
+	// SIGINT is the signal to terminate ("interrupt") the process.
+	signal.Notify(ch, syscall.SIGINT)
+
+	// Setup the server
+	srv := server.New()
+
+	// Run the server, but in its own goroutine without blocking this thread.
+	go func() {
+		if err := srv.Listen(*addr); err != nil {
+			os.Exit(1)
+		}
+	}()
+
+	// Block, awaiting the signal
+	<-ch
+
+	if err := srv.Shutdown(); err != nil {
+		os.Exit(1)
+	}
+
+	os.Exit(0)
+}
+
+func init() {
+	flag.Parse()
 }
