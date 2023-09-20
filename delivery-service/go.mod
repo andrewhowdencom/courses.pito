a4fdc08486a1ebe2b59b6fac0321e04f3e2eb3c2
Andrew Howden <hello@andrewhowden.com>
2023-09-20 17:40:07 +0200

    Instrument the HTTP APIs
    This commit applies the automated instrumentation to the HTTP APIs.
    
    == Design Notes
    === Limited Use
    
    The metrics generated by default are of limited use, as they only break
    up the routes as a status code and not by the route name (or similar).
    Need to look into how to fix that.
    
    Sample metrics:
    
    1. http_server_duration_bucket
    2. http_server_duration_sum
    3. http_server_duration_count
    4. http_server_request_content_length_total
    5. http_server_response_content_length_total

--- delivery-service/go.mod
+++ delivery-service/go.mod
@@ -3,6 +3,7 @@ module github.com/andrewhowdencom/courses.pito/delivery-service
 go 1.21.0
 
 require (
+	github.com/prometheus/client_golang v1.16.0
 	go.opentelemetry.io/otel v1.18.0
 	go.opentelemetry.io/otel/exporters/prometheus v0.41.0
 	go.opentelemetry.io/otel/sdk/metric v0.41.0
@@ -11,14 +12,16 @@ require (
 require (
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/felixge/httpsnoop v1.0.3 // indirect
 	github.com/go-logr/logr v1.2.4 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/matttproud/golang_protobuf_extensions v1.0.4 // indirect
-	github.com/prometheus/client_golang v1.16.0 // indirect
 	github.com/prometheus/client_model v0.4.0 // indirect
 	github.com/prometheus/common v0.42.0 // indirect
 	github.com/prometheus/procfs v0.10.1 // indirect
+	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.44.0 // indirect
+	go.opentelemetry.io/contrib/instrumentation/runtime v0.44.0 // indirect
 	go.opentelemetry.io/otel/metric v1.18.0 // indirect
 	go.opentelemetry.io/otel/sdk v1.18.0 // indirect
 	go.opentelemetry.io/otel/trace v1.18.0 // indirect
--- delivery-service/server/server.go
+++ delivery-service/server/server.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/andrewhowdencom/courses.pito/delivery-service/carriers"
+	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
 )
 
 type Server struct {
@@ -19,13 +20,26 @@ func New() *Server {
 	srv := &Server{}
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/healthz", srv.healthz)
-	mux.HandleFunc("/delivery-options", srv.deliveryOptions)
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusServiceUnavailable)
-		w.Write([]byte("Service Unavailable\n"))
-	})
+	// The binding of the method to the routes includes the "instrumentation middleware". The first example is
+	// explained in depth, with the subsequent handlers repeated.
+	mux.Handle(
+		// The first argument is the route. In this case, "healthz" following Googles conventions.
+		// See
+		// 1. https://stackoverflow.com/a/43381061
+		"/healthz",
 
+		// The second argument is the handler. However, the actual handler is wrapped in the middleware handler
+		// that instruments the request and response.
+		otelhttp.NewHandler(
+			// The first argument converts the "handler func" to a "handler"
+			http.HandlerFunc(srv.healthz),
+
+			// The second argument will be used as the operation name in the metrics and traces.
+			"healthz",
+		),
+	)
+
+	mux.Handle("/delivery-options", otelhttp.NewHandler(http.HandlerFunc(srv.deliveryOptions), "delivery-options"))
 	return &Server{
 		srv: &http.Server{
 			Handler: mux,
